094ec74a542896bc44f3fc097b198ca0491b4a7e
Kendeki <92603580+Kendeki@users.noreply.github.com>
2024-09-06 18:29:44 -0300

    Add files via upload
    comentario eh coisa de viado

--- jogodavelha.py
+++ jogodavelha.py
@@ -5,19 +5,26 @@ import funcoesjogo as fj
 jogo = [['  '] * 3 for _ in range(3)] # Cria uma lista repleta de "  " para servir de tabuleiro
 game_on = True # Status do jogo
 bot = False # Define se será contra bot ou não
-diff = False # Define a dificuldade do bot
+diff = False # Define a dificuldade do bot (False = Fácil, True = Difícil)
 turn = True # True = X, False = O
 
-question = int(input("1 - Modo de 2 jogadores\n2 - Modo contra bot\nR: "))
-if question == 2:
-  bot = True
-  question = int(input("1 - Modo fácil\n2 - Modo difícil\nR: "))
-  if question == 2:
-    diff = True
+while True:
+  question = int(input("1 - Modo de 2 jogadores\n2 - Modo contra bot\nR: "))
+  if question != 1 and question != 2:
+    print("Escolha inválida.")
+  elif question == 2:
+    bot = True
+    while True:
+      question = int(input("1 - Modo fácil\n2 - Modo difícil\nR: "))
+      if question != 1 and question != 2:
+        print("Escolha inválida.")
+      elif question == 2:
+        diff = True
+        break
+    break
 
-while game_on: # Executa o jogo enquanto não houver um vencedor ou empatar 
+while game_on:
   
-  # vvv Imprime o tabuleiro na tela
   tabuleiro = f"""{jogo[0][0]} | {jogo[0][1]} | {jogo[0][2]}
 ------------
 {jogo[1][0]} | {jogo[1][1]} | {jogo[1][2]}
@@ -26,28 +33,27 @@ while game_on: # Executa o jogo enquanto não houver um vencedor ou empatar
 """
   
   print(f"\n{tabuleiro}\n")
-  conditions = [[" X", " X", " X"], [" O", " O", " O"]] # Condições de vitória
+  conditions = [[" X", " X", " X"], [" O", " O", " O"]]
   almost = [[" X", " X", "  "], [" X", "  ", " X"], ["  ", " X", " X"]]
-  
-  if not bot: # Loop para o modo de 2 jogadores
-  
-    if fj.win(jogo):
-      if not turn:
-        print("X Ganhou!")
-        game_on = False
-        break
-        
-      else:
-        print("O Ganhou!")
-        game_on = False
-        break
-        
+
+  if fj.win(jogo):
+    if not turn:
+      print("X Ganhou!")
+      game_on = False
+      break
+
     else:
-      if fj.empate(jogo):
-        print("Empate.")
-        game_on = False
-        break
-        
+      print("O Ganhou!")
+      game_on = False
+      break
+
+  else:
+    if fj.empate(jogo):
+      print("Empate.")
+      game_on = False
+      break
+  
+  if not bot:
     while True:
       x, y = int(input("Insira a linha: ")), int(input("Insira a coluna: "))
   
@@ -69,25 +75,9 @@ while game_on: # Executa o jogo enquanto não houver um vencedor ou empatar
       
     turn = not(turn)
     
-  else: # Loop para o modo contra bot
+  else:
     if not diff:
-      if fj.win(jogo):
-        if not turn:
-          print("X Ganhou!")
-          game_on = False
-          break
-        else:
-          print("O Ganhou!")
-          game_on = False
-          break
-      else:
-        if fj.empate(jogo):
-          print("Empate.")
-          game_on = False
-          break
-  
       while True:
-  
         if turn:
           x, y = int(input("Insira a linha: ")), int(input("Insira a coluna:"))
           
@@ -107,25 +97,7 @@ while game_on: # Executa o jogo enquanto não houver um vencedor ou empatar
             
       turn = not(turn)
 
-    else:
-    
-      if fj.win(jogo):
-        if not turn:
-          print("X Ganhou!")
-          game_on = False
-          break
-    
-        else:
-          print("O Ganhou!")
-          game_on = False
-          break
-    
-      else:
-        if fj.empate(jogo):
-          print("Empate.")
-          game_on = False
-          break
-          
+    else:   
       while True:
         x, y = rd.randint(0, 2), rd.randint(0, 2)
       
